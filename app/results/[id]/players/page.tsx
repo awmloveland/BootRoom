53b96489aec289b116910d3d949ccc1681decfcd
Will Loveland <awmloveland@gmail.com>
2026-04-16 14:12:43 +0100

    feat: redirect /results/[uuid]/players to /[slug]/players

---
+++ app/results/[id]/players/page.tsx
@@ -0,0 +1,15 @@
+export const dynamic = 'force-dynamic'
+
+import { notFound, redirect } from 'next/navigation'
+import { getGame } from '@/lib/fetchers'
+
+interface Props {
+  params: Promise<{ id: string }>
+}
+
+export default async function LegacyResultsPlayersPage({ params }: Props) {
+  const { id } = await params
+  const game = await getGame(id)
+  if (!game) notFound()
+  redirect(`/${game.slug}/players`)
+}
